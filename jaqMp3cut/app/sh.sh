44c8df3eff5661a00d4354e8d00c9f24396a337c
andrey <lamzin.an@gmail.com>
2021-01-09 21:44:26 +0300

    Команда для mp3 extract в cmd.txt

--- jaqMp3cut/app/sh.sh
+++ jaqMp3cut/app/sh.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
-cd /home/andrey/tmp/00/11;
-ffmpeg -i strelnikoff-2020-02-28-22-35-00.mp3 -ss 00:16:09 -t 00:01:17 strelnikoff-2020-02-28-22-35-00-out.mp3
+cd /media/andrey/Transcend/HBPVR;
+ffmpeg -i new_year-09-01-2021.mp3 -ss 00:00:00 -t 00:02:00 new_year-09-01-2021-out.mp3
